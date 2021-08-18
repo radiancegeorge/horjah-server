a72b3c271906e94f583407db6636304018d729df
radiancegeorge <radiancegeorge@outlook.com>
2021-08-18 21:26:48 +0100

    live db

--- middlewares/admin.auth.middleware.js
+++ middlewares/admin.auth.middleware.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const adminProtect = (req, res, next) => {
   req.user.email === process.env.admin_email
     ? next()
-    : res.status(401).json("invalid token");
+    : res.status(401).json({ error: "invalid token" });
 };
 
 module.exports = adminProtect;
